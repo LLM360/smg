0bb524f09c886b6b1dbfcbcc8338bc2dd189e007
Chang Su <chang.s.su@oracle.com>
2026-04-15 13:40:45 -0700

    Revert "Revert "feat(image): add image generation tool support" (#1087)"
    This reverts commit d09cb630, re-adding image generation tool support
    from PR #1057. Resolved conflicts with current main and added missing
    `internal` field on McpServerConfig.
    
    Signed-off-by: Chang Su <chang.s.su@oracle.com>

--- model_gateway/src/routers/grpc/common/responses/utils.rs
+++ model_gateway/src/routers/grpc/common/responses/utils.rs
@@ -47,7 +47,9 @@ pub(crate) async fn ensure_mcp_connection(
             t.iter().any(|tool| {
                 matches!(
                     tool,
-                    ResponseTool::WebSearchPreview(_) | ResponseTool::CodeInterpreter(_)
+                    ResponseTool::WebSearchPreview(_)
+                        | ResponseTool::CodeInterpreter(_)
+                        | ResponseTool::ImageGeneration(_)
                 )
             })
         })
